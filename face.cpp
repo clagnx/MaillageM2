72100a4e4213ff0a3c6378fa632a3bf2d37f9b1d
clagnx <cla.gangneux@gmail.com>
2017-09-06 17:19:34 +0200

    coucou, on affiche des points youhou

--- face.cpp
+++ face.cpp
@@ -9,20 +9,24 @@ Face::Face(){
 }
 
 Face::Face(Point* p1, Point* p2, Point* p3){
-    p1 = p1;
-    p2 = p2;
-    p3 = p3;
+    this->p1 = p1;
+    this->p2 = p2;
+    this->p3 = p3;
 }
 
 void Face::draw(){
     glBegin(GL_POINTS);
 
-    std::cout<<getP1()->getX()<<std::endl;
+    std::cout<<getP2()->getX()<<std::endl;
 
     glVertex3f(getP1()->getX(), getP1()->getY(),  getP1()->getZ());
     glVertex3f(getP2()->getX(), getP2()->getY(),  getP2()->getZ());
     glVertex3f(getP3()-> getX(), getP3()-> getY(),  getP3()->getZ());
 
+   /* glVertex3f(0.0, 0.0,  0.0);
+    glVertex3f(1.0, 1.0,  0.0);
+    glVertex3f(2.0, 2.0,  0.0);
+*/
     glEnd();
 }
 
